7161c291953e0d87c9617628cb1f186705c8a718
Q <q.st000rm@gmail.com>
2022-01-25 01:57:08 +0800

    Day16

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,15 +1,18 @@
+#include <cstdint>
 #include<iostream>
-#include<bitset>
+#include<vector>
 
 using namespace std;
 int idx = 0;
 string hexStr;
-int parse();
+pair<int, long long> parse();
+long long calc(int id, vector<long long> &nums);
 int main(){
 	cin >> hexStr;
 	int versionSum = 0;
-	int res =parse();
-	cout << "Part I answer:\n" << res << endl;
+	auto res =parse();
+	cout << "Part I answer:\n" << res.first << endl;
+	cout << "Part II answer:\n" << res.second << endl;
 	return 0;
 }
 int get(int n){
@@ -23,37 +26,82 @@ int get(int n){
 	idx += n;
 	return res;
 }
-int parse(){
-	pair<int, int> ans;
-	int val = 0;
+pair<int, long long> parse(){
+	pair<int, long long> ans;
+	long long val = 0;
 	int version = get(3);
 	int Typeid = get(3);
 	/* cout << "version: " << version << " | type: " << Typeid << endl; */
 	if(Typeid == 4){
-		int lit;
+		long long lit;
 		while((lit = get(5)) & 16){
-			val = 8 * val + lit % 16;
+			val =  (val << 4) + lit % 16 * 1LL;
 		}
-		val = 8 * val + lit % 16;
+		val = (val << 4) + lit % 16 * 1LL;
 	}
 	else{
 		//Sub-packet parse:
-		/* version += get(3); */
-		/* Typeid = get(3); */
 		int I = get(1);
 		int len;
+		vector<long long> packet;
 		if(I){
 			len = get(11);
 			for(int i = 0; i < len; i++){
-				version += parse();
+				auto p = parse();
+				version += p.first;
+				packet.push_back(p.second);
 			}
+			val = calc(Typeid, packet);
 		}else{
 			len = get(15);
 			int cur_idx = idx;
 			while(idx < cur_idx + len){
-				version +=parse();
+				auto p = parse();
+				version += p.first;
+				packet.push_back(p.second);
 			}
+			val = calc(Typeid, packet);
 		}
 	}
-	return version;
+	if(val < 0)
+		cout << val << endl;
+	return {version, val};
+}
+long long calc(int id, vector<long long> &nums){
+	if(id == 0){
+		long long sum = 0;
+		for(auto num: nums)
+			sum += num;
+		return sum;
+	}
+	if(id == 1){
+		long long product = 1;
+		for(auto num: nums)
+			product *= num;
+		return product;
+	}
+	if(id == 2){
+		long long min_num = INT64_MAX;
+		for(auto num: nums)
+			min_num = min(min_num, num);
+		if(min_num == INT64_MAX)
+			cout <<"????\n";
+		return min_num;
+	}
+	if(id == 3){
+		long long max_num = INT64_MIN;
+		for(auto num: nums)
+			max_num = max(max_num, num);
+		if(max_num == INT32_MIN)
+			cout << "????\n";
+		return max_num;
+	}
+	if(id == 5)
+		return nums[0] > nums[1];
+	if(id == 6)
+		return nums[0] < nums[1];
+	if(id == 7)
+		return nums[0] == nums[1];
+	cout << "Errrrro\n";
+	return 0;
 }
